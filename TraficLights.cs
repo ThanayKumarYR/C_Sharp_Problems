36ee1529466892f138a79da0b04ef995d43ac5d3
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-03-14 15:27:30 +0530

    9th problem

---
+++ TraficLights.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace problems
+{
+    class TraficLights
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to trafic signal !");
+            Random rad = new Random();
+            if (rad.Next(1, 4) == 1)
+            { 
+                Console.WriteLine("It is 'RED', please stop !");
+            }
+            else if (rad.Next(1, 4) == 2)
+            {
+                Console.WriteLine("It is 'YELLOW', please 'GET READY' !");
+
+            }
+            else if (rad.Next(1, 4) == 3)
+            {
+                Console.WriteLine("It is 'GREEN', You can 'GO' !");
+            }
+        }
+    }
+}
